c0a127e6c32724817464dca0f3fcf8f825e5c756
Nicolas Enriquez <nicolasaaronenriquez95@gmail.com>
2021-11-04 21:35:58 -0300

    reestructuracion de repositorio

---
+++ src/components/title/title.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class TitleComponent extends Component {
+    render() { 
+        return <div>
+            <h1>Hola</h1>
+        </div>;
+    }
+}
+ 
+export default TitleComponent;
