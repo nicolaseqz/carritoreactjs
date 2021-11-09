395adc6ba738ebd794c64844711ee3e0dd70585d
Nicolas Enriquez <nicolasaaronenriquez95@gmail.com>
2021-11-09 20:10:18 -0300

    fin de desafio n2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navbar/navbar';
+import Catalogo from './components/catalogo/catalogo';
+import ItemListContainer from './components/itemlistcontainer';
+
 
 function App() {
   return (
     <div className="App">
-      NavBar
+      <NavBar/>
+      <Catalogo/>
+      <ItemListContainer/>
     </div>
   );
 }
 
-export default NavBar;
+export default App;
---
+++ src/components/itemlistcontainer.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+const ItemListContainer = ({saludo}) => {
+    return (
+        <div>
+            <h1>Hola mundo!</h1>
+        </div>
+    )
+}
+
+export default ItemListContainer;
