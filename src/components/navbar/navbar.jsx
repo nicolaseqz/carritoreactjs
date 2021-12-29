e83f308751275399c9e9a2caafc70750f9162418
Nicolas Enriquez <nicolasaaronenriquez95@gmail.com>
2021-12-29 19:51:25 -0300

    ultima actualizacion pre fin de curso

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
+    <div class="Barra">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/categorias/procesadores">Procesadores</Link>
